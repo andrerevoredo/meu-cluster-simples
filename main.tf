788d77c651730a82884c296ce2f3d579d3d382e4
Andre <andre.revoredo@hotmail.com>
2025-08-02 20:53:25 -0300

    ajusta .gitignore

--- main.tf
+++ main.tf
@@ -11,15 +11,13 @@ module "vpc" {
   name = "vpc-${var.cluster_name}"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
   
-  # enable_dns_support = true
-  # enable_dns_hostnames = true
   tags = {
     Terraform   = "true"
     Environment = "dev"
@@ -53,9 +51,9 @@ module "eks" {
   
   enable_cluster_creator_admin_permissions = true
 
-  eks_managed_node_group_defaults = {
-    instance_types = ["t3.small"]
-  }
+  # eks_managed_node_group_defaults = {
+  #   instance_types = ["t3.small"]
+  # }
 
   eks_managed_node_groups = {
     
