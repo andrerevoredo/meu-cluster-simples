2ac9376420a4f6d2964c703c3d073cf87aabd276
Andre <andre.revoredo@hotmail.com>
2025-08-02 20:48:18 -0300

    ajusta codigo

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
   
-  enable_dns_support = true
-  enable_dns_hostnames = true
+  # enable_dns_support = true
+  # enable_dns_hostnames = true
   tags = {
     Terraform   = "true"
     Environment = "dev"
@@ -39,24 +39,18 @@ module "vpc" {
 # Criação do Cluster EKS
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  #version = "21.0.5"
+  
   version = "20.8.4"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
   
-  #name = var.cluster_name
-  #kubernetes_version = "1.30"
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   # Para tornar o cluster acessível pela internet
   cluster_endpoint_public_access = true
   
-  #endpoint_public_access = true
-  #endpoint_private_access = true
-
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_group_defaults = {
